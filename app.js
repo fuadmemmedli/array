24ecd47ac08df288a8e8257a8917ac14a50fbb49
fuadmemmedli <tu7nfntsk@code.edu.az>
2022-11-03 11:19:29 +0400

    js code added

---
+++ app.js
@@ -0,0 +1,10 @@
+const arr = new Array(4);
+for (let i = 0; i < arr.length; i++) {
+console.log();(myNum[i] + ", ");
+}
+//3cu
+/*for(int j = 0; j < myNum.Length; j++)
+{
+    myNum[j]++;
+    console.log(myNum[j]);
+}*/
